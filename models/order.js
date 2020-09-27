b8bfef58b073735c15312fcbaf6724ac0b823e3f
keizoku <uliana.sulaimanov@bk.ru>
2020-09-27 06:06:26 +0600

    Add additional fields to order model

--- models/order.js
+++ models/order.js
@@ -11,6 +11,34 @@ const order = sequelize.define('order', {
     quantity: {
         type: Sequelize.INTEGER,
         defaultValue: 1
+    },
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    productName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    discount: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    payService: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    cartNumber: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
 });
 
