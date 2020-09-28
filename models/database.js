e81eea292e1530427e749ddd719f0d09eee68d74
keizoku <uliana.sulaimanov@bk.ru>
2020-09-28 21:55:46 +0600

    refactor: Change project database to postgresql

--- models/database.js
+++ models/database.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize');
 const logger = require('../middleware/logger');
 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false });
+
+/*const sequelize = new Sequelize(process.env.DATABASE_URL, {
     logging: msg => logger.info(msg),
-});
+	mysql://root:keiz@localhost:3306/online_shop
+});*/
 
 
 module.exports = sequelize;
